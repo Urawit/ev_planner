a237facfe3d2ffd5e72d031ec5423488fdc666df
Urawit <urawit.j@ku.th>
2025-02-14 22:55:46 +0700

    feat: ui progression

--- lib/features/sign_in/presentation/sign_in_page.dart
+++ lib/features/sign_in/presentation/sign_in_page.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../shared/theme/ev_design_system.dart';
 
-class SignInPage extends StatelessWidget {
+class SignInPage extends ConsumerWidget {
   const SignInPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            //TODO make String Constant
-            Padding(
-              padding: const EdgeInsets.only(bottom: 40),
-              child: Text('Welcome to EV Planner',
-                  style: EVDesignSystem.textStyles.headline1),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 27, bottom: 40),
+                child: Text('Welcome !',
+                    style: EVDesignSystem.textStyles.headline1),
+              ),
             ),
             const SizedBox(
-                width: 300,
+                width: 327,
                 child: TextField(
                   obscureText: true,
                   decoration: InputDecoration(
@@ -27,9 +30,9 @@ class SignInPage extends StatelessWidget {
                     labelText: 'Email',
                   ),
                 )),
-            const SizedBox(height: 20),
+            const SizedBox(height: 24),
             const SizedBox(
-                width: 300,
+                width: 327,
                 child: TextField(
                   obscureText: true,
                   decoration: InputDecoration(
@@ -37,6 +40,22 @@ class SignInPage extends StatelessWidget {
                     labelText: 'Password',
                   ),
                 )),
+            const SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                        style: null,
+                        onPressed: () {
+                          ref.read()
+                        },
+                        child: const Text('Login')),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
